af1bf6b56bb3ba3a966015abd6fb570d58f2d275
Gus Ballman <gus.ballman@wsu.edu>
2025-09-23 13:18:21 -0700

    Update PA2.c

--- PA 2 & 3 Music Player/PA2.c
+++ PA 2 & 3 Music Player/PA2.c
@@ -1,5 +1,12 @@
 /*
-
+	Programmer: Gus Ballman
+	Class: CptS 122, Lab Section 01
+	Date: 9/22/2025
+	Description: Program reads music record data (artist name, song name, album name, genre name, rating, timesplayed, length of the song) from a csv file and will allow the user to select from 11 differnt options. Option one loads the information from the file 
+	into a doubly linked list and insert each new line of data into its own record node at the front of teh linked list. Option two stores the data that is in the linked list into the csv file after deleting the cvs file's previous contents. Option 3 will display 
+	either all of the records stored in the linked list, or one artist in which the user chooses. Option 4 will allow the user to manually insert a new record into the linked list. Option 5 allows teh user to delete a record from the linked list, Option 6 will allow 
+	the user to edit any attribute of a record. Option 7 will allow the user to sort the data by artist name, album name, timesplayed, or the rating of the song. Option 8 allows the user to edit teh rating of any particular song. Option 9 allows the user to display
+	the songs in the order that they are given (this mimicks a play feature). Option 10 randomly shuffles the order of the songs. Option 11 stores the stored records into the csv file and will terminate the program.
 */
 
 #include "PA2.h"
@@ -764,4 +771,5 @@ bool loadSort(FILE* infile, Node** headPtr, int* count, sortType type)
 		}
 	}
 	return success;
+
 }
